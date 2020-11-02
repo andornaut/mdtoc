c02b2b4fa7fb9fce202083e7b7f1cbb6575469b0
andornaut <andornaut@users.noreply.github.com>
2020-11-02 10:49:39 -0500

    Extract CLI module

--- lib/mdtoc.rb
+++ lib/mdtoc.rb
@@ -3,6 +3,7 @@
 
 require 'optparse'
 require 'tempfile'
+require_relative 'mdtoc/cli'
 require_relative 'mdtoc/markdown'
 require_relative 'mdtoc/node'
 require_relative 'mdtoc/writer'
@@ -10,26 +11,6 @@ require_relative 'mdtoc/writer'
 module Mdtoc
   class << self
     extend T::Sig
-
-    sig { params(args: T::Array[String]).returns(T::Hash[Symbol, String]) }
-    def parse_args!(args)
-      options = {}
-      OptionParser.new do |parser|
-        parser.banner = "Usage: #{parser.program_name} [options] files or directories..."
-        parser.on('-h', '--help', 'Show this message') do
-          puts parser
-          exit
-        end
-        parser.on('-o', '--output PATH', 'Update a table of contents in the file at PATH')
-        parser.on('-a', '--[no-]append', 'Append to the --output file if a <!-- mdtoc --> tag isn\'t found')
-        parser.on('-c', '--[no-]create', 'Create the --output file if it does not exist')
-      end.parse!(args, into: options)
-      if args.empty?
-        warn('Specify at least one file or directory to read')
-        exit(1)
-      end
-      options
-    end
 
     sig { params(paths: T::Array[String]).returns(String) }
     def render_toc(paths)
@@ -42,17 +23,14 @@ module Mdtoc
 
     sig { params(args: T::Array[String]).void }
     def main(args)
-      options = parse_args!(args)
-      toc = render_toc(args)
-      output_path = options[:output]
-      unless output_path
+      options = Mdtoc::CLI::Options.for_args(args)
+      toc = render_toc(options.paths)
+      unless options.output_path
         puts toc
         return
       end
 
-      append = T.cast(options[:append] || false, T::Boolean)
-      create = T.cast(options[:create] || false, T::Boolean)
-      Mdtoc::Writer.write(toc, output_path, append, create)
+      Mdtoc::Writer.write(toc, T.must(options.output_path), options.append, options.create)
     end
   end
 end
---
+++ lib/mdtoc/cli.rb
@@ -0,0 +1,45 @@
+# typed: true
+# frozen_string_literal: true
+
+require 'optparse'
+require 'sorbet-runtime'
+require 'tempfile'
+require_relative 'markdown'
+require_relative 'node'
+require_relative 'writer'
+
+module Mdtoc
+  module CLI
+    extend T::Helpers
+
+    class Options < T::Struct
+      extend T::Sig
+
+      prop :append, T::Boolean, default: false
+      prop :create, T::Boolean, default: false
+      prop :output_path, T.nilable(String)
+      prop :paths, T::Array[String], default: []
+
+      sig { params(args: T::Array[String]).returns(Options) }
+      def self.for_args(args)
+        parser = OptionParser.new do |parser_|
+          parser_.banner = "Usage: #{parser_.program_name} [options] files or directories..."
+          parser_.on('-h', '--help', 'Show this message') do
+            puts parser_
+            exit
+          end
+          parser_.on('-o', '--output PATH', 'Update a table of contents in the file at PATH')
+          parser_.on('-a', '--[no-]append', 'Append to the --output file if a <!-- mdtoc --> tag isn\'t found')
+          parser_.on('-c', '--[no-]create', 'Create the --output file if it does not exist')
+        end
+        options = Options.new
+        options.paths = parser.parse(args, into: options)
+        if options.paths.empty?
+          warn('Specify at least one file or directory to read')
+          exit(1)
+        end
+        options
+      end
+    end
+  end
+end
